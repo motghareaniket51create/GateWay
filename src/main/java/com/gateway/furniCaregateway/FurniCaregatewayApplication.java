9df51ede94a6590584775bef67792b393b5a1f6e
motghareaniket51create <motghareaniket51@gmail.com>
2026-04-16 21:45:36 +0530

    main changes

--- src/main/java/com/gateway/furniCaregateway/FurniCaregatewayApplication.java
+++ src/main/java/com/gateway/furniCaregateway/FurniCaregatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FurniCaregatewayApplication {
 
+	// API gateway
 	public static void main(String[] args) {
 		SpringApplication.run(FurniCaregatewayApplication.class, args);
 	}
