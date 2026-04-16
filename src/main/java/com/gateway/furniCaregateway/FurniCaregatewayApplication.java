b00622a3fd549361bd4e72c619187f25b426c008
motghareaniket51create <motghareaniket51@gmail.com>
2026-04-16 21:44:29 +0530

    Added the comments

--- src/main/java/com/gateway/furniCaregateway/FurniCaregatewayApplication.java
+++ src/main/java/com/gateway/furniCaregateway/FurniCaregatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FurniCaregatewayApplication {
 
+	// This is the API gateway
 	public static void main(String[] args) {
 		SpringApplication.run(FurniCaregatewayApplication.class, args);
 	}
