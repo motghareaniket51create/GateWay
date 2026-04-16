c76d322a76f17bb5274b2aebdb63c134ea448a37
motghareaniket51create <motghareaniket51@gmail.com>
2026-04-16 21:41:36 +0530

    Initial commits

---
+++ src/main/java/com/gateway/furniCaregateway/FurniCaregatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.furniCaregateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurniCaregatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurniCaregatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gateway/furniCaregateway/FurniCaregatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.furniCaregateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniCaregatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
